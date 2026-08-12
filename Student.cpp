be60f6cbd957bbcb74cec2f30422a7a104086853
saipatil1102 <saip73565@gmail.com>
2026-08-12 05:33:39 +0000

    assignment 3

---
+++ Student.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+void inputDetails();
+class Student
+{
+private:
+    string name;
+    int rollNo;
+    float marks;
+
+// public:
+    void inputDetails()
+    {
+        cout << "Enter Student Name: ";
+        getline(cin >> ws, name);
+
+        cout << "Enter Roll Number: ";
+        cin >> rollNo;
+
+        cout << "Enter Marks: ";
+        cin >> marks;
+    }
+public:
+    void displayDetails() const
+    {
+        inputDetails();
+        cout << "\n----- Student Details -----\n";
+        cout << "Name    : " << name << endl;
+        cout << "Roll No.: " << rollNo << endl;
+        cout << "Marks   : " << marks << endl;
+    }
+};
+
+int main()
+{
+    Student s;
+
+    inputDetails();
+   // s.rollNo=100;
+    s.displayDetails();
+
+    return 0;
+}
+
+void inputDetails()
+{
+    cout<<"I am calling a method outside the class ";
+}
