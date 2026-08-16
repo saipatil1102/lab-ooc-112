e1808d6dbf71c87b56f8fc3fb18a6afd8c23919c
saipatil1102 <saip73565@gmail.com>
2026-08-16 11:57:42 +0000

    larger between two numbers

---
+++ Expirements_1/Que9.cpp
@@ -0,0 +1,23 @@
+# include<iostream>
+using namespace std ;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter value of a :";
+    cin>>a;
+    cout<<"enter value of b :";
+    cin>>b;
+
+    if(a>b)
+    {
+        cout<<"a is grater than b";
+
+    }
+    else
+    {
+        cout<<"b is grater than a";
+    }
+    return 0;
+    
+}
