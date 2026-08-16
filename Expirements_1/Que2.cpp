78aec68793cac673c55f89d7e5e645e052679774
saipatil1102 <saip73565@gmail.com>
2026-08-16 10:40:33 +0000

    hello world msg

---
+++ Expirements_1/Que2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,sum=0;
+    cout<<"Enter two numbers :";
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"Sum of two numbers is :"<<sum;
+    return 0;
+}
