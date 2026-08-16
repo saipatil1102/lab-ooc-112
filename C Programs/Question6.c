6b347f21358252b5e1e60a44bdde09ad953230d6
saipatil1102 <saip73565@gmail.com>
2026-08-16 09:32:18 +0000

    Question6 addition of  two numbers

---
+++ C Programs/Question6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,sum=0;
+    printf("Enter two numbers : ");
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    printf("Sum of two numbers is %d",sum);     
+    return 0;
+}
